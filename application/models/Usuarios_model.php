71f4b13b061c1bd28a86391054b550f6a774af3e
Alejandro Rodriguez <alerd6539@gmail.com>
2018-10-23 11:37:38 -0500

    Correccion Login y DB

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -4,8 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Usuarios_model extends CI_Model {
 	function login($username, $password){
 		$this->db->where("nombre_usuario",$username);
-		$this->db->where("password",$password);
-		$resultados = $this->db->get("rp_usuarios");
+		$this->db->where("user_contrasenia",$password);
+		$resultados = $this->db->get("rici_usuarios");
 		if ($resultados->num_rows() > 0) {
 			return $resultados->row();
 		}else{
